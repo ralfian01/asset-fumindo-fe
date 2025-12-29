5b5ae9c847e097f51638c80318c360b43e7bf8f8
Rifky Alfian <ralfian096.project@gmail.com>
2025-12-30 01:28:56 +0700

    add: sidebar components

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -19,11 +19,11 @@ const emit = defineEmits(['close'])
 
     <!-- Sidebar Main -->
     <aside
-      class="fixed inset-y-0 left-0 z-30 w-64 bg-slate-900 text-white transform transition-transform duration-300 lg:translate-x-0 lg:static lg:inset-0"
+      class="fixed min-h-[100vh] inset-y-0 left-0 z-30 w-64 bg-red-900 text-white transform transition-transform duration-300 lg:translate-x-0 lg:static lg:inset-0"
       :class="isOpen ? 'translate-x-0' : '-translate-x-full'"
     >
       <!-- Logo Area -->
-      <div class="flex items-center justify-between h-16 px-6 bg-slate-800">
+      <div class="flex items-center justify-between h-16 px-6 bg-[#590303]">
         <span class="text-xl font-bold tracking-wider">FumindoApp</span>
         <button @click="emit('close')" class="lg:hidden text-gray-400 hover:text-white">
           <X :size="24" />
@@ -34,8 +34,8 @@ const emit = defineEmits(['close'])
       <nav class="mt-5 px-4 space-y-2">
         <RouterLink
           to="/"
-          class="flex items-center px-4 py-3 text-gray-300 transition-colors rounded-lg hover:bg-slate-800 hover:text-white"
-          active-class="bg-blue-600 text-white shadow-lg"
+          class="flex items-center px-4 py-3 text-gray-300 transition-colors rounded-lg hover:bg-red-800 hover:text-white"
+          active-class="bg-primary text-white shadow-lg"
         >
           <LayoutDashboard :size="20" class="mr-3" />
           Dashboard
@@ -43,8 +43,8 @@ const emit = defineEmits(['close'])
 
         <RouterLink
           to="/assets"
-          class="flex items-center px-4 py-3 text-gray-300 transition-colors rounded-lg hover:bg-slate-800 hover:text-white"
-          active-class="bg-blue-600 text-white shadow-lg"
+          class="flex items-center px-4 py-3 text-gray-300 transition-colors rounded-lg hover:bg-red-800 hover:text-white"
+          active-class="bg-primary text-white shadow-lg"
         >
           <Box :size="20" class="mr-3" />
           Data Aset
